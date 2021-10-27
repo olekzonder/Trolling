acada81b584e43648656f70b65b28198cf1c5e91
Alexander Smirnov <35576482+olekzonder@users.noreply.github.com>
2021-10-27 13:21:07 +0300

    файлы троленга

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Trolling
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(103, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Хотите автомат по всем предметам?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ХОЧУ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(503, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Не хочу...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 277);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Заманчивое предложение...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trolling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.MouseHover += button1_MouseHover;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int _x = 650;
+        int _y = 160;
+        Random rnd = new Random();
+        Point tmp_location;
+
+        int _w = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width - 100;
+        int _h = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height / 2;
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            _x += rand.Next(-200,200);
+            _y += rand.Next(-50,50);
+            _w += rand.Next(-250, 250);
+            _h += rand.Next(-250, 250);
+            //Чтобы кнопки не улетали
+            _x = Math.Abs(_x % 650);
+            _y = Math.Abs(_y % 160);
+            _w = Math.Abs(_w % (System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width-100));
+            _h = Math.Abs(_h % (System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height/2));
+            button1.Location = new Point(_x, _y);
+            this.Location = new Point(_w, _h);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("А ты усердный");
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ну и правильно", "Верный выбор", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Exit();
+        }
+    }
+}
